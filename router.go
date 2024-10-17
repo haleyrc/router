b8fafd425cb3d74161a626e0826791bc3373638b
Ryan Haley <me@ryanchaley.com>
2024-10-17 13:55:19 -0400

    feat: Add an initial router implementation
    This commit includes a wrapper around the stdlib http.ServeMux that
    provides slightly nicer methods for registering routes.

---
+++ router.go
@@ -0,0 +1,34 @@
+// Package router provides an http.Handler implementation with a nice API for
+// registering routes.
+package router
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Router is a wrapper around an http.ServeMux that provides a slightly nicer
+// API for registering routes.
+type Router struct {
+	mux *http.ServeMux
+}
+
+// New creates a new Router.
+func New() Router {
+	return Router{mux: http.NewServeMux()}
+}
+
+// GET registers a route at the provided path that responds to GET requests.
+func (router Router) GET(path string, h http.Handler) {
+	router.mux.Handle(fmt.Sprintf("GET %s", path), h)
+}
+
+// POST registers a route at the provided path that responds to POST requests.
+func (router Router) POST(path string, h http.Handler) {
+	router.mux.Handle(fmt.Sprintf("POST %s", path), h)
+}
+
+// ServeHTTP implements the http.Handler interface.
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router.mux.ServeHTTP(w, r)
+}
